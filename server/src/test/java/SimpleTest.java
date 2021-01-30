768a68910de1ff46b3f227fe44e3b938eaa9a7b4
Tim Weißenfels <tim.weissenfels@outlook.com>
2021-01-31 00:40:37 +0100

    Basic Unit Test example #6

---
+++ server/src/test/java/SimpleTest.java
@@ -0,0 +1,9 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SimpleTest {
+    @Test
+    void myAwesomeTest() {
+        assertEquals(2, 1+1);
+    }
+}

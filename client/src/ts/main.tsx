c742072cbf25b846cdae730d70a7c4bfa07374b2
Tim Weißenfels <tim.weissenfels@outlook.com>
2021-02-05 16:28:17 +0100

    Fix vertex buffer layout

--- client/src/ts/main.tsx
+++ client/src/ts/main.tsx
@@ -13,23 +13,31 @@ document.body.appendChild(canvas);
 gl.clearColor(0.66, 0.85, 0.8, 1.0);
 gl.clear(gl.COLOR_BUFFER_BIT);
 
-// transformation matrix (un-stretch)
+// transformation matrix (un-stretch & scale)
 const unstretch = new Matrix();
 unstretch.setEntry(0, 0, canvas.height / canvas.width);
+
+const scale = new Matrix();
+scale.setEntry(0,0, 0.25);
+scale.setEntry(1,1, 0.25);
+
+const transform = Matrix.compose(scale, unstretch);
 
 // create GPU buffer
 const vertexBuffer = gl.createBuffer();
 gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
 
 const program = Renderer.createSnakeShaderProgram(gl);
+program.bufferLayout = ["vPosition", "vLength", "vCenterOffset"];
+console.log(program.bufferLayout);
 program.use();
 program.setUniform("uColor", [0.2, 0.4, 1.0]);
-program.setUniform("uTransform", unstretch.data);
+program.setUniform("uTransform", transform.data);
 
 const worker = new Worker("worker.bundle.js", { name: "SnakeWorker" });
 worker.postMessage({
-    tag: "ConnectToServer",
-    playerName: "SnakeForceOne",
+    tag: "RunTest",
+    //playerName: "SnakeForceOne",
 } as MessageFromMain);
 worker.postMessage({
     tag: "UpdateTargetAlpha",
@@ -40,5 +48,6 @@ worker.addEventListener("message", (event) => {
     console.log("Snake Chunk data!")
     const data = event.data.data as SnakeChunkData;
     gl.bufferData(gl.ARRAY_BUFFER, data.glVertexBuffer, gl.STATIC_DRAW);
+    gl.clear(gl.COLOR_BUFFER_BIT);
     program.run(gl.TRIANGLE_STRIP, 0, data.vertices);
 });

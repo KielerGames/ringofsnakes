ccec9522c983c7fdfbc0e3426ee9a2f69e81581d
Tim Weißenfels <tim.weissenfels@outlook.com>
2021-02-10 16:49:05 +0100

    UserInput visualization improvements

--- client/src/ts/components/UserInput.tsx
+++ client/src/ts/components/UserInput.tsx
@@ -7,27 +7,30 @@ type Props = {
 
 type State = {
     alpha: number;
+    lastChange: number;
 };
 
 export default class UserInput extends Component<Props, State> {
     private ref = React.createRef<HTMLDivElement>();
+    private timeout: number | undefined;
 
     public constructor(props: Props) {
         super(props);
-        this.state = { alpha: props.initial ?? 0.0 };
+        this.state = { alpha: props.initial ?? 0.0, lastChange: -Infinity };
 
         this.clickHandler = this.clickHandler.bind(this);
         this.mouseMoveHandler = this.mouseMoveHandler.bind(this);
     }
 
     public render() {
+        const time = (performance.now() - this.state.lastChange) / 1000;
         return (
             <div
                 id="input-container"
                 ref={this.ref}
                 onClick={this.clickHandler}
             >
-                <div id="input-viz-ring">
+                <div id="input-viz-ring" className={time > 1 ? "hide" : ""}>
                     <div
                         id="input-viz-marker"
                         style={{ transform: `rotate(${-this.state.alpha}rad)` }}
@@ -57,18 +60,30 @@ export default class UserInput extends Component<Props, State> {
                     return {
                         alpha:
                             x === 0.0 && y === 0.0 ? alpha : Math.atan2(y, x),
+                        lastChange: performance.now(),
                     };
                 } else {
                     const x = e.pageX - 0.5 * window.innerWidth;
                     const y = 0.5 * window.innerHeight - e.pageY;
 
-                    return { alpha: Math.atan2(y, x) };
+                    return {
+                        alpha: Math.atan2(y, x),
+                        lastChange: performance.now(),
+                    };
                 }
             },
             () => {
                 if (this.props.onChange) {
                     this.props.onChange(this.state.alpha);
                 }
+                if (this.timeout !== undefined) {
+                    window.clearTimeout(this.timeout);
+                    this.timeout = undefined;
+                }
+                this.timeout = window.setTimeout(() => {
+                    this.timeout = undefined;
+                    this.forceUpdate();
+                }, 1000);
             }
         );
     }

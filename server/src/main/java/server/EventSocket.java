27604f043315af542f1d5c4e0002b0336cc00e32
Tim Weißenfels <tim.weissenfels@outlook.com>
2021-02-12 00:15:46 +0100

    Remove unused closureLatch

--- server/src/main/java/server/EventSocket.java
+++ server/src/main/java/server/EventSocket.java
@@ -5,13 +5,10 @@ import javax.websocket.server.ServerEndpoint;
 import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.util.Locale;
-import java.util.concurrent.CountDownLatch;
 
 @ClientEndpoint
 @ServerEndpoint(value = "/game")
 public class EventSocket {
-    private CountDownLatch closureLatch = new CountDownLatch(1);
-
     @OnOpen
     public void onWebSocketConnect(Session session) {
         System.out.println("Socket Connected: " + session);
@@ -38,18 +35,10 @@ public class EventSocket {
     public void onWebSocketClose(Session session, CloseReason reason) {
         System.out.println("Socket Closed: " + reason);
         SnakeServer.removePlayer(session);
-
-        // TODO: what is the closureLatch for?
-        closureLatch.countDown();
     }
 
     @OnError
     public void onWebSocketError(Throwable cause) {
         cause.printStackTrace(System.err);
-    }
-
-    public void awaitClosure() throws InterruptedException {
-        System.out.println("Awaiting closure from remote");
-        closureLatch.await();
     }
 }

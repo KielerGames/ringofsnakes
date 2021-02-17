783d96006d6da8508068d555ee26513eee79dd71
Tim Weißenfels <tim.weissenfels@outlook.com>
2021-02-17 18:36:32 +0100

    Add common interface for SnakeChunk and SnakeChunkBuilder

--- server/src/main/java/game/snake/SnakeChunk.java
+++ server/src/main/java/game/snake/SnakeChunk.java
@@ -3,21 +3,23 @@ package game.snake;
 import math.BoundingBox;
 import java.nio.ByteBuffer;
 
-public class SnakeChunk {
+public class SnakeChunk implements SnakeChunkData {
     public final static int BYTE_SIZE = 128;
     public final static int HEADER_BYTE_SIZE = 21;
     public final static int BUFFER_N_POS = 4;
     public final static int BUFFER_OFFSET_POS = 17;
 
-    public ByteBuffer chunkByteBuffer;
+    private final Snake snake;
+    private ByteBuffer chunkByteBuffer;
     private double length;
 
     private BoundingBox boundingBox;
 
-    protected SnakeChunk(ByteBuffer buffer, BoundingBox box, double length) {
+    protected SnakeChunk(Snake snake, ByteBuffer buffer, BoundingBox box, double length) {
         assert buffer.position() == BYTE_SIZE;
         assert length > 0;
 
+        this.snake = snake;
         chunkByteBuffer = buffer;
         boundingBox = box;
         this.length = length;
@@ -25,5 +27,17 @@ public class SnakeChunk {
 
     public ByteBuffer getBuffer() {
         return chunkByteBuffer.asReadOnlyBuffer().flip();
+    }
+
+    public Snake getSnake() {
+        return this.snake;
+    }
+
+    public int getByteSize() {
+        return BYTE_SIZE;
+    }
+
+    public boolean isEmpty() {
+        return false;
     }
 }

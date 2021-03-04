93531cdc9a430fdc905f0e63ec3f2d7f7153ca15
Tim Weißenfels <tim.weissenfels@outlook.com>
2021-03-04 14:37:48 +0100

    Add self collision config flag

--- server/src/main/java/game/GameConfig.java
+++ server/src/main/java/game/GameConfig.java
@@ -4,6 +4,7 @@ public final class GameConfig {
     public final double snakeSpeed;
     public final double fastSnakeSpeed;
     public final double maxTurnDelta;
+    public final boolean selfCollision = false;
 
     public GameConfig() {
         snakeSpeed = 0.24;

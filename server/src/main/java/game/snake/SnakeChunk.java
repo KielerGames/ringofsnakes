f8595d4e898767ddf9d8d16fc06ce55916c50560
kilian2 <kilian.kkl@googlemail.com>
2021-03-10 21:05:55 +0100

    implemented collision detection
    in Player send Update printing was out commented

--- server/src/main/java/game/snake/SnakeChunk.java
+++ server/src/main/java/game/snake/SnakeChunk.java
@@ -41,8 +41,10 @@ public class SnakeChunk implements SnakeChunkData {
         initPointData(points);
 
         //some primitive testing of the collision detection, needs to be deleted later
-        assert(doesCollideWith(snake.headPosition, 10) ==  doesCollideWith(snake.headPosition, points, 10));
-        if(doesCollideWith(snake.headPosition, points, 10)){System.out.println("Collision");}
+        assert (doesCollideWith(snake.headPosition, 10) == doesCollideWith(snake.headPosition, points, 10));
+        if (doesCollideWith(snake.headPosition, points, 10)) {
+            System.out.println("Collision");
+        }
 
     }
 
@@ -63,21 +65,21 @@ public class SnakeChunk implements SnakeChunkData {
         //System.out.println("Debug");
     }
 
-    private int getGridColumn(Vector point){
+    private int getGridColumn(Vector point) {
         double chunkWidth = boundingBox.maxX - boundingBox.minX;
-        double relativePosition =  (point.x - boundingBox.minX)/chunkWidth;
-        int column =  (relativePosition != 1 ? (int)(relativePosition * gridSideLength) : (int)(relativePosition * gridSideLength));
+        double relativePosition = (point.x - boundingBox.minX) / chunkWidth;
+        int column = (relativePosition != 1 ? (int) (relativePosition * gridSideLength) : (int) (relativePosition * gridSideLength));
         return column;
     }
 
-    private int getGridRow(Vector point){
+    private int getGridRow(Vector point) {
         double chunkWidth = boundingBox.maxY - boundingBox.minY;
-        double relativePosition =  (point.y - boundingBox.minY)/chunkWidth;
-        int row =  (relativePosition != 1 ? (int)(relativePosition * gridSideLength) : (int)(relativePosition * gridSideLength));
+        double relativePosition = (point.y - boundingBox.minY) / chunkWidth;
+        int row = (relativePosition != 1 ? (int) (relativePosition * gridSideLength) : (int) (relativePosition * gridSideLength));
         return row;
     }
 
-    private byte getHashFromRowAndColumn(int row, int column){
+    private byte getHashFromRowAndColumn(int row, int column) {
         int hash = gridSideLength * column + row;
         return (byte) hash;
     }
@@ -96,44 +98,52 @@ public class SnakeChunk implements SnakeChunkData {
         int rightHash, topHash, leftHash, bottomHash;
         rightHash = topHash = leftHash = bottomHash = -4242;
 
-        if(rightEdge.x <= boundingBox.maxX){
+        if (rightEdge.x <= boundingBox.maxX) {
             int gridRow = getGridRow(rightEdge);
             int gridColumn = getGridColumn(rightEdge);
             rightHash = getHashFromRowAndColumn(gridRow, gridColumn);
-            List<Vector> points = pointData.get((byte)rightHash);
-            if(points != null){
-                for (Vector p : points) {potentialPositionPoints.add(p);}
+            List<Vector> points = pointData.get((byte) rightHash);
+            if (points != null) {
+                for (Vector p : points) {
+                    potentialPositionPoints.add(p);
+                }
             }
 
         }
-        if(topEdge.y <= boundingBox.maxY){
+        if (topEdge.y <= boundingBox.maxY) {
             int hash = getHashFromRowAndColumn(getGridRow(topEdge), getGridColumn(topEdge));
-            if(hash != rightHash){
+            if (hash != rightHash) {
                 topHash = hash;
-                List<Vector> points = pointData.get((byte)topHash);
-                if(points != null){
-                    for (Vector p : points) {potentialPositionPoints.add(p);}
+                List<Vector> points = pointData.get((byte) topHash);
+                if (points != null) {
+                    for (Vector p : points) {
+                        potentialPositionPoints.add(p);
+                    }
                 }
 
             }
         }
-        if(leftEdge.x >= boundingBox.minX){
+        if (leftEdge.x >= boundingBox.minX) {
             int hash = getHashFromRowAndColumn(getGridRow(leftEdge), getGridColumn(leftEdge));
-            if(hash != rightHash && hash != topHash) {
+            if (hash != rightHash && hash != topHash) {
                 leftHash = hash;
-                List<Vector> points = pointData.get((byte)leftHash);
-                if(points != null){
-                    for (Vector p : points) {potentialPositionPoints.add(p);}
+                List<Vector> points = pointData.get((byte) leftHash);
+                if (points != null) {
+                    for (Vector p : points) {
+                        potentialPositionPoints.add(p);
+                    }
                 }
             }
         }
-        if(bottomEdge.y >= boundingBox.minY){
+        if (bottomEdge.y >= boundingBox.minY) {
             int hash = getHashFromRowAndColumn(getGridRow(bottomEdge), getGridColumn(bottomEdge));
-            if(hash != rightHash && hash != topHash && hash != leftHash){
+            if (hash != rightHash && hash != topHash && hash != leftHash) {
                 bottomHash = hash;
-                List<Vector> points = pointData.get((byte)bottomHash);
-                if(points != null){
-                    for (Vector p : points) {potentialPositionPoints.add(p);}
+                List<Vector> points = pointData.get((byte) bottomHash);
+                if (points != null) {
+                    for (Vector p : points) {
+                        potentialPositionPoints.add(p);
+                    }
                 }
             }
         }
@@ -141,9 +151,9 @@ public class SnakeChunk implements SnakeChunkData {
         return doesCollideWith(position, potentialPositionPoints, radius);
     }
 
-    public boolean doesCollideWith(Vector position, List<Vector> points, double radius){
-        for(Vector p : points){
-            if((position.x - p.x)*(position.x - p.x) + (position.y - p.y)*(position.y - p.y) < radius*radius){
+    public boolean doesCollideWith(Vector position, List<Vector> points, double radius) {
+        for (Vector p : points) {
+            if ((position.x - p.x) * (position.x - p.x) + (position.y - p.y) * (position.y - p.y) < radius * radius) {
                 return true;
             }
         }

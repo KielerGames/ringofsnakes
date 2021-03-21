b270456c42d2d895f2915160de490cf3407baf6c
Tim Weißenfels <tim.weissenfels@outlook.com>
2021-03-21 23:34:04 +0100

    BoundingBox distance function

--- client/src/ts/math/BoundingBox.ts
+++ client/src/ts/math/BoundingBox.ts
@@ -1,15 +1,13 @@
+/**
+ * Represents an axis-aligned rectangle.
+ */
 export default class BoundingBox {
     public minX: number;
     public maxX: number;
     public minY: number;
     public maxY: number;
 
-    public constructor(
-        minX: number,
-        maxX: number,
-        minY: number,
-        maxY: number
-    ) {
+    public constructor(minX: number, maxX: number, minY: number, maxY: number) {
         this.minX = minX;
         this.maxX = maxX;
         this.minY = minY;
@@ -35,6 +33,31 @@ export default class BoundingBox {
 
     public static fromTransferable(obj: TransferableBox): BoundingBox {
         return new BoundingBox(obj.minX, obj.maxX, obj.minY, obj.maxY);
+    }
+
+    /**
+     * Computes the squared distance between two BoundingBox instances.
+     * @param a A BoundingBox
+     * @param b Another BoundingBox
+     * @returns Squared distance
+     */
+    public static distance2(a: BoundingBox, b: BoundingBox): number {
+        let dx = 0.0,
+            dy = 0.0;
+
+        if (a.maxX < b.minX) {
+            dx = b.minX - a.maxX;
+        } else if (b.maxX < a.minX) {
+            dx = a.minX - b.maxX;
+        }
+
+        if (a.maxY < b.minY) {
+            dy = b.minY - a.maxY;
+        } else if (b.maxY < a.minY) {
+            dy = a.minY - b.maxY;
+        }
+
+        return dx*dx + dy*dy;
     }
 }
 

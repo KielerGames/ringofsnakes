6f137dd7feb7b845131abef79974cf0ae6b6e082
Tim Weißenfels <tim.weissenfels@outlook.com>
2021-04-20 19:45:47 +0200

    More tests

--- client/src/tests/decoder.spec.ts
+++ client/src/tests/decoder.spec.ts
@@ -3,22 +3,25 @@ import { assert } from "chai";
 import * as SCD from "../app/worker/decoder/SnakeChunkDecoder";
 import { GameConfig } from "../app/protocol";
 
-
 const cfg: GameConfig = {
     snakeSpeed: 0.24,
     fastSnakeSpeed: 0.48,
     maxTurnDelta: Math.PI / 30,
-    tickDuration: 1.0/25
+    tickDuration: 1.0 / 25,
 };
+
+function createSnakeChunkBuffer(numberOfChainCodes: number): ArrayBuffer {
+    const data = new Uint8Array(
+        SCD.SNAKE_CHUNK_HEADER_SIZE + numberOfChainCodes
+    );
+    data[4] = numberOfChainCodes;
+    return data.buffer;
+}
 
 describe("SnakeChunkDecoder", () => {
     it("should accept valid buffers", () => {
-        assert.doesNotThrow(() => {
-            const numberOfChainCodes = 1;
-            const data = new Uint8Array(SCD.SNAKE_CHUNK_HEADER_SIZE + numberOfChainCodes);
-            data[4] = numberOfChainCodes;
-            SCD.decode(data.buffer, 0, cfg);
-        });
+        const buffer = createSnakeChunkBuffer(1);
+        assert.doesNotThrow(() => SCD.decode(buffer, 0, cfg));
     });
 
     it("should reject buffers that are too small", () => {
@@ -30,9 +33,38 @@ describe("SnakeChunkDecoder", () => {
         assert.throws(() => {
             const numberOfChainCodes = 2;
             // create a buffer that is too small...
-            const data = new Uint8Array(SCD.SNAKE_CHUNK_HEADER_SIZE + numberOfChainCodes - 1);
+            const data = new Uint8Array(
+                SCD.SNAKE_CHUNK_HEADER_SIZE + numberOfChainCodes - 1
+            );
             data[4] = numberOfChainCodes;
             SCD.decode(data.buffer, 0, cfg);
         }, RangeError);
+
+        assert.throws(() => {
+            const buffer = createSnakeChunkBuffer(1);
+            SCD.decode(buffer, 13, cfg);
+        }, RangeError);
+    });
+
+    it("pathLength should behave linearly", () => {
+        const buffer = createSnakeChunkBuffer(42);
+        const pathLength = SCD.decode(buffer, 0, cfg).data.pathLength;
+        assert.isAbove(pathLength, 0.0);
+
+        const fasterCfg: GameConfig = Object.assign({}, cfg, { snakeSpeed: 4.2 * cfg.snakeSpeed });
+        assert.approximately(SCD.decode(buffer, 0, fasterCfg).data.pathLength, 4.2 * pathLength, 1e-8);
+    });
+
+    it("pathData length component should increase monotonically", () => {
+        const buffer = createSnakeChunkBuffer(42);
+        const pathData = SCD.decode(buffer, 0, cfg).data.pathData;
+        assert.equal(pathData.length % 4, 0, "pathData length should be a multiple of 4");
+        
+        let lastLC = -1.0;
+        for(let i=0; i<pathData.length; i += 4) {
+            const lc = pathData[i + 2];
+            assert.isAbove(lc, lastLC);
+            lastLC = lc;
+        }
     });
 });

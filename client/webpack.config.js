ed81966d19da3799e58919ab979d2aab5ca367d5
Tim Weißenfels <tim.weissenfels@outlook.com>
2021-04-22 12:11:51 +0200

    Compile only bundled files

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -8,14 +8,16 @@ const shaders = {};
 
 console.log("Loading shaders...");
 let shaderFiles = fs.readdirSync(SHADER_DIR);
-shaderFiles.forEach(filename => {
-    if(filename.endsWith(".vert") || filename.endsWith(".frag")) {
+shaderFiles.forEach((filename) => {
+    if (filename.endsWith(".vert") || filename.endsWith(".frag")) {
         console.log(" --> " + filename);
         let content = fs.readFileSync(SHADER_DIR + "/" + filename, "utf8");
         content = JSON.stringify(content.trim());
 
-        let stn = filename.endsWith(".vert") ? "VERTEXSHADER" : "FRAGMENTSHADER";
-        let id  = filename.slice(0, filename.length-5).toUpperCase();
+        let stn = filename.endsWith(".vert")
+            ? "VERTEXSHADER"
+            : "FRAGMENTSHADER";
+        let id = filename.slice(0, filename.length - 5).toUpperCase();
 
         shaders["__" + stn + "_" + id + "__"] = content;
     }
@@ -26,7 +28,7 @@ module.exports = {
     mode: "development",
     entry: {
         main: path.join(__dirname, "src", "app", "main.tsx"),
-        worker: path.join(__dirname, "src", "app", "worker", "worker.ts")
+        worker: path.join(__dirname, "src", "app", "worker", "worker.ts"),
     },
     target: "web",
     devtool: "eval-cheap-module-source-map",
@@ -37,16 +39,23 @@ module.exports = {
         rules: [
             {
                 test: /\.tsx?$/,
-                use: "ts-loader",
-                exclude: "/node_modules/",
+                loader: "ts-loader",
+                options: {
+                    onlyCompileBundledFiles: true,
+                },
             },
         ],
     },
     plugins: [
-        new webpack.DefinePlugin(Object.assign({
-            __VERSION__: JSON.stringify(`${pkg.version}-dev`),
-            __DEBUG__: "true",
-        }, shaders)),
+        new webpack.DefinePlugin(
+            Object.assign(
+                {
+                    __VERSION__: JSON.stringify(`${pkg.version}-dev`),
+                    __DEBUG__: "true",
+                },
+                shaders
+            )
+        ),
     ],
     output: {
         filename: "[name].bundle.js",
--- client/webpack.config.production.js
+++ client/webpack.config.production.js
@@ -8,14 +8,16 @@ const shaders = {};
 
 console.log("Loading shaders...");
 let shaderFiles = fs.readdirSync(SHADER_DIR);
-shaderFiles.forEach(filename => {
-    if(filename.endsWith(".vert") || filename.endsWith(".frag")) {
+shaderFiles.forEach((filename) => {
+    if (filename.endsWith(".vert") || filename.endsWith(".frag")) {
         console.log(" --> " + filename);
         let content = fs.readFileSync(SHADER_DIR + "/" + filename, "utf8");
         content = JSON.stringify(content.trim());
 
-        let stn = filename.endsWith(".vert") ? "VERTEXSHADER" : "FRAGMENTSHADER";
-        let id  = filename.slice(0, filename.length-5).toUpperCase();
+        let stn = filename.endsWith(".vert")
+            ? "VERTEXSHADER"
+            : "FRAGMENTSHADER";
+        let id = filename.slice(0, filename.length - 5).toUpperCase();
 
         shaders["__" + stn + "_" + id + "__"] = content;
     }
@@ -26,29 +28,36 @@ module.exports = {
     mode: "production",
     entry: {
         main: path.join(__dirname, "src", "app", "main.tsx"),
-        worker: path.join(__dirname, "src", "app", "worker", "worker.ts")
+        worker: path.join(__dirname, "src", "app", "worker", "worker.ts"),
     },
     target: "web",
     resolve: {
-        extensions: [".ts", ".tsx", ".js"]
+        extensions: [".ts", ".tsx", ".js"],
     },
     module: {
         rules: [
             {
                 test: /\.tsx?$/,
-                use: "ts-loader",
-                exclude: "/node_modules/"
-            }
+                loader: "ts-loader",
+                options: {
+                    onlyCompileBundledFiles: true,
+                },
+            },
         ],
     },
     plugins: [
-        new webpack.DefinePlugin(Object.assign({
-            __VERSION__: JSON.stringify(pkg.version),
-            __DEBUG__: "false",
-        }, shaders)),
+        new webpack.DefinePlugin(
+            Object.assign(
+                {
+                    __VERSION__: JSON.stringify(pkg.version),
+                    __DEBUG__: "false",
+                },
+                shaders
+            )
+        ),
     ],
     output: {
         filename: "[name].bundle.js",
         path: path.resolve(__dirname, "public"),
     },
-}
+};

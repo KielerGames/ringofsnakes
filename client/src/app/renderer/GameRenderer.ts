8707e9db0c6bf545e2f3e1519b5ad3d9922cec0b
Tim Weißenfels <tim.weissenfels@outlook.com>
2021-05-13 21:39:52 +0200

    Handle page resize #18

--- client/src/app/renderer/GameRenderer.ts
+++ client/src/app/renderer/GameRenderer.ts
@@ -4,6 +4,8 @@ import Matrix from "../math/Matrix";
 import * as SnakeChunkRenderer from "./SnakeChunkRenderer";
 import * as SnakeHeadRenderer from "./SnakeHeadRenderer";
 
+declare const __DEBUG__: boolean;
+
 let gl: WebGLRenderingContext;
 
 const unstretch = new Matrix();
@@ -11,26 +13,29 @@ const scale = new Matrix();
 const translate = new Matrix();
 
 export function init(parentNode: HTMLElement = document.body): void {
+    // create canvas element
     const canvas = document.createElement("canvas");
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
-    unstretch.setEntry(0, 0, canvas.height / canvas.width);
+    parentNode.appendChild(canvas);
 
+    // init gl context
     gl = canvas.getContext("webgl", {
         alpha: false,
         antialias: true,
         preserveDrawingBuffer: false,
     })!;
-
     gl.disable(gl.DEPTH_TEST);
 
-    parentNode.appendChild(canvas);
+    resize(true);
 
+    // init other render modules
     SnakeChunkRenderer.init(gl);
     SnakeHeadRenderer.init(gl);
 }
 
 export function render(data: GameData, camera: Camera, time: number): void {
+    // resize canvas (only if needed)
+    resize();
+
     // background color
     gl.clearColor(0.1, 0.1, 0.1, 1.0);
     gl.clear(gl.COLOR_BUFFER_BIT);
@@ -56,4 +61,28 @@ export function render(data: GameData, camera: Camera, time: number): void {
 
     // render snake heads
     SnakeHeadRenderer.render(data.getSnakes(), transform, pTime);
+}
+
+function resize(force: boolean = false) {
+    const canvas = gl.canvas as HTMLCanvasElement;
+
+    // get current canvas size in CSS pixels
+    const displayWidth = canvas.clientWidth;
+    const displayHeight = canvas.clientHeight;
+
+    const needResize =
+        canvas.width !== displayWidth || canvas.height !== displayHeight;
+
+    if (force || needResize) {
+        if (__DEBUG__) {
+            console.info("Resizing canvas...");
+        }
+        // resize canvas
+        canvas.width = displayWidth;
+        canvas.height = displayHeight;
+        // update clip space to screen pixel transformation
+        gl.viewport(0, 0, displayWidth, displayHeight);
+        // update aspect ratio
+        unstretch.setEntry(0, 0, displayHeight / displayWidth);
+    }
 }

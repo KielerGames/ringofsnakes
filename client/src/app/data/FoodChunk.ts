4c83277f0df8ab0abce27cfcd1d22ad632eda971
Tim Weißenfels <tim.weissenfels@outlook.com>
2021-09-05 15:31:14 +0200

    remove unused method

--- client/src/app/data/FoodChunk.ts
+++ client/src/app/data/FoodChunk.ts
@@ -8,18 +8,4 @@ export default class FoodChunk {
         this.id = id;
         this.food = food;
     }
-
-    public static createRandom(): FoodChunk {
-        const foods = new Array(20);
-
-        for (let i = 0; i < foods.length; i++) {
-            const x = (2 * Math.random() - 1) * 25;
-            const y = (2 * Math.random() - 1) * 25;
-            foods[i] = new Food(x, y, 0.5 + Math.random(), 0);
-        }
-
-        const id = Math.floor(Math.random() * 4200);
-
-        return new FoodChunk(id, foods);
-    }
 }

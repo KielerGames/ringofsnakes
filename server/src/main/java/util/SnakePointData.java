6d2bdd668194a7c50b0f21045b70d348709bf56e
kilian2 <kilian.kkl@googlemail.com>
2021-09-06 22:38:49 +0200

    This is helpful for the changes in Snake.java for collision detection.

---
+++ server/src/main/java/util/SnakePointData.java
@@ -0,0 +1,15 @@
+package util;
+
+import math.Vector;
+
+public class SnakePointData{
+    public final Vector point;
+    public final boolean fast;
+
+    public SnakePointData(Vector point, boolean fast){
+        this.point = point;
+        this.fast = fast;
+    }
+
+
+}

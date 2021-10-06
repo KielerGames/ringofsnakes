8d88ecdbcde6c6ebfa261a541d0ece0718924d26
Tim Weißenfels <tim.weissenfels@outlook.com>
2021-10-06 20:18:38 +0200

    Fix format

--- client/src/app/renderer/SnakeHeadRenderer.ts
+++ client/src/app/renderer/SnakeHeadRenderer.ts
@@ -51,7 +51,7 @@ export function render(
     shader.setUniform("uTransform", transform.data);
 
     for (const snake of snakes) {
-        if(!snake.hasChunks()) {
+        if (!snake.hasChunks()) {
             continue;
         }
 

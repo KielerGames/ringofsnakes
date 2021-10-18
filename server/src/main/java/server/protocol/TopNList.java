96510e98873fd06906ca68525ab62811ac4264a1
kilian2 <kilian.kkl@googlemail.com>
2021-10-19 00:04:18 +0200

    save progress

---
+++ server/src/main/java/server/protocol/TopNList.java
@@ -0,0 +1,32 @@
+package server.protocol;
+
+import game.Game;
+import game.snake.Snake;
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class TopNList extends ServerToClientJSONMessage{
+
+    public final short[] ids;
+    public final int[] scores;
+
+    public TopNList(Game game, int n){
+        final var length = Math.min(n, game.snakes.size());
+        ids = new short[length];
+        scores = new int[length];
+
+        final var topSnakes = game.snakes.stream().filter(Snake::isAlive)
+                .sorted(Comparator.comparing(Snake::getLength)
+                        .reversed()).collect(Collectors.toList());
+
+        int i = 0;
+        for (Snake snake : topSnakes) {
+            if(i < n) {
+                ids[i] = snake.id;
+                scores[i]= (int) snake.getLength();
+            }
+            i++;
+        }
+    }
+}
+

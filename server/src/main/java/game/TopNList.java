a149d13a63186daa439e6c6c38323c6249802807
kilian2 <kilian.kkl@googlemail.com>
2021-10-18 22:04:10 +0200

    save progress

--- server/src/main/java/game/TopNList.java
+++ server/src/main/java/game/TopNList.java
@@ -1,5 +1,6 @@
 package game;
 
+import com.google.gson.Gson;
 import game.snake.Snake;
 
 import java.util.*;
@@ -7,10 +8,16 @@ import java.util.stream.Collectors;
 
 public class TopNList {
 
-    public final List<Short> ids = new LinkedList();
-    public final List<Integer> scores = new LinkedList();
+    public final short[] ids;
+    public final int[] scores;
+
+    Gson gson = new Gson();
 
     public TopNList(Game game, int n){
+        final var length = Math.min(n, game.snakes.size());
+        ids = new short[length];
+        scores = new int[length];
+
         final var topSnakes = game.snakes.stream().filter(Snake::isAlive)
                 .sorted(Comparator.comparing(Snake::getLength)
                         .reversed()).collect(Collectors.toList());
@@ -18,18 +25,20 @@ public class TopNList {
         int i = 0;
         for (Snake snake : topSnakes) {
             if(i < n) {
-                ids.add(snake.id);
-                scores.add((int) snake.getLength());
+                ids[i] = snake.id;
+                scores[i]= (int) snake.getLength();
             }
             i++;
         }
     }
 
+    public String getJson = gson.toJson(this);
+
     @Override
     public String toString() {
         return "TopNList{" +
-                "ids=" + ids +
-                ", scores=" + scores +
+                "ids=" + Arrays.toString(ids) +
+                ", scores=" + Arrays.toString(scores) +
                 '}';
     }
 }

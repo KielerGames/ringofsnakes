3188d55c8c01d17eb695ef89698a555ad9137c3c
kilian2 <kilian.kkl@googlemail.com>
2021-10-19 14:00:40 +0200

    keep the bots in the map

--- server/src/main/java/game/ai/Bot.java
+++ server/src/main/java/game/ai/Bot.java
@@ -8,10 +8,14 @@ import math.Vector;
 public abstract class Bot {
     private final Game game;
     private Snake snake;
+    private final double worldHeight;
+    private final double worldWidth;
 
     public Bot(Game game, Vector spawnPosition) {
         this.game = game;
         this.snake = SnakeFactory.createSnake(spawnPosition, game.world);
+        this.worldHeight = game.world.height;
+        this.worldWidth = game.world.width;
     }
 
     public Snake getSnake() {
@@ -20,6 +24,14 @@ public abstract class Bot {
 
     public boolean isAlive() {
         return snake.isAlive();
+    }
+
+    public double getWorldHeight(){
+        return this.worldHeight;
+    }
+
+    public double getWorldWidth() {
+        return this.worldWidth;
     }
 
     public abstract void act();

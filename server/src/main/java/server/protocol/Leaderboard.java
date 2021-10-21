a784476651fec882c85dc0225ce202a2fd6620f4
kilian2 <kilian.kkl@googlemail.com>
2021-10-22 00:41:50 +0200

    players only

--- server/src/main/java/server/protocol/Leaderboard.java
+++ server/src/main/java/server/protocol/Leaderboard.java
@@ -2,6 +2,8 @@ package server.protocol;
 
 import game.Game;
 import game.snake.Snake;
+import server.Player;
+
 import java.util.*;
 import java.util.stream.Collectors;
 
@@ -10,7 +12,9 @@ public class Leaderboard extends ServerToClientJSONMessage{
     private final List<LeaderboardEntry> list;
 
     public Leaderboard(Game game, int n){
-        list = game.snakes.stream().filter(Snake::isAlive)
+         list = game.streamClients().filter(Player.class::isInstance)
+                .map(client -> ((Player) client).snake)
+                .filter(Snake::isAlive)
                 .sorted(Comparator.comparing(Snake::getLength).reversed())
                 .limit(Math.min(n, game.snakes.size()))
                 .map(LeaderboardEntry::new)

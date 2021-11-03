3555cc1498ba35d9e396c929909106248473ef32
Tim Weißenfels <tim.weissenfels@outlook.com>
2021-11-03 21:10:52 +0100

    Add JSON loading utility

---
+++ client/src/app/utilities/JSONLoader.ts
@@ -0,0 +1,37 @@
+type TypeGuard<T> = (data: any) => data is T;
+
+const defaultGuard = function <T>(data: any): data is T {
+    // the default guard accepts any data
+    return true;
+};
+
+type LoadOptions<T> = {
+    allowCache?: boolean;
+    guard?: TypeGuard<T>;
+};
+
+export async function loadJSON<T>(
+    path: string,
+    options: LoadOptions<T> = {}
+): Promise<T> {
+    // initialize options
+    const allowCache = options.allowCache ?? false;
+    const guard = options.guard ?? defaultGuard;
+
+    const response = await fetch(path, {
+        method: "GET",
+        cache: allowCache ? "default" : "no-store",
+    });
+
+    if (!response.ok) {
+        return Promise.reject(`Request failed (${path})`);
+    }
+
+    const data = await response.json();
+
+    if (guard(data)) {
+        return data;
+    } else {
+        return Promise.reject("Invalid data.");
+    }
+}

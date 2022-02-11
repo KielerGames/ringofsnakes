ea159c0824068d68ca76410f5baf8e80cba5e711
Tim Weißenfels <tim-we@users.noreply.github.com>
2022-02-11 17:30:28 +0100

    remove SnakeList debug UI

--- client/src/app/ui/GameOverlay.tsx
+++ client/src/app/ui/GameOverlay.tsx
@@ -1,7 +1,6 @@
 import { Component } from "preact";
 import Game from "../data/Game";
 import FPSStats from "./components/debug/FPSStats";
-import SnakeList from "./components/debug/SnakeList";
 import Leaderboard from "./components/Leaderboard";
 import SnakeInfoUI from "./components/SnakeInfoUI";
 
@@ -15,7 +14,7 @@ export default class GameOverlay extends Component<Props> {
     componentDidMount() {
         this.timer = window.setInterval(() => {
             this.forceUpdate();
-        }, 100);
+        }, 500);
     }
 
     componentWillUnmount() {
@@ -24,14 +23,12 @@ export default class GameOverlay extends Component<Props> {
 
     render() {
         const game = this.props.game;
-        const snakeSupplier = () => game.snakes.values();
 
         return (
             <>
                 <Leaderboard data={game.leaderboard} />
                 <FPSStats />
                 <SnakeInfoUI snake={game.targetSnake} />
-                {__DEBUG__ ? <SnakeList supplier={snakeSupplier} /> : null}
             </>
         );
     }

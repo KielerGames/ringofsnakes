25eb97727c9e06eb38e2b7193fd654e3750a2749
Tim Weißenfels <tim-we@users.noreply.github.com>
2022-06-24 17:29:48 +0200

    Fix name generator test

--- server/src/test/java/game/NameGeneratorTest.java
+++ server/src/test/java/game/NameGeneratorTest.java
@@ -5,17 +5,20 @@ import org.junit.jupiter.api.Test;
 import java.util.HashSet;
 import java.util.Random;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class NameGeneratorTest {
     @Test
     void testGenerateMultipleUniqueNames() {
+        final int n = 16;
+
         final var rand = new Random(20220624);
         final var names = new HashSet<String>();
 
-        for (int i = 0; i < 16; i++) {
-            final var uniqueName = NameGenerator.generateUnique(rand, names);
-            names.add(uniqueName);
+        for (int i = 0; i < n; i++) {
+            names.add(NameGenerator.generateUnique(rand, names));
         }
 
-        System.out.println(names);
+        assertEquals(n, names.size());
     }
 }

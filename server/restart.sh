5e77d5d7e022780365ec2cc18336051908c6d3ce
Tim Weißenfels <tim-we@users.noreply.github.com>
2022-08-13 02:01:58 +0200

    Restart game server

---
+++ server/restart.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# This script is part of our ci pipeline and is used to start new versions.
+# Do not use it to run the game locally unless you know what you are doing ;)
+
+# stop previous versions
+pkill -f java
+
+# start server in screen
+screen -d -m -S snakeserver java -jar snake-server.jar

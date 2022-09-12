72d5ba708322ae73679aecd42bd562c7ee05aaf0
Tim Weißenfels <tim-we@users.noreply.github.com>
2022-09-13 00:39:22 +0200

    Convert AppEvent.ts

--- client/src/app/util/AppEvent.ts
+++ client/src/app/util/AppEvent.ts
@@ -1,13 +1,13 @@
 import { Consumer } from "./FunctionTypes";
 
 export class AppEvent<T = void> {
-    private readonly listeners = new Set<Consumer<T>>();
+    readonly #listeners = new Set<Consumer<T>>();
 
     addListener(listener: Consumer<T>): void {
-        this.listeners.add(listener);
+        this.#listeners.add(listener);
     }
 
     trigger(data: T): void {
-        this.listeners.forEach((listener) => listener(data));
+        this.#listeners.forEach((listener) => listener(data));
     }
 }

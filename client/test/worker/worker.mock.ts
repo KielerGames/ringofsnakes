bf132f268286dab038f52dae3aaec019dd8e2ade
Tim Weißenfels <tim-we@users.noreply.github.com>
2022-09-15 01:30:55 +0200

    Fix test

--- client/test/worker/worker.mock.ts
+++ client/test/worker/worker.mock.ts
@@ -3,6 +3,7 @@ import defaultGameInfo from "../data/dto/GameInfo.prefab";
 const defaultRemoteMock = {
     init: () => Promise.resolve(defaultGameInfo),
     addEventListener: jest.fn(),
+    onSpectatorChange: jest.fn(),
     getDataChanges: jest.fn()
 };
 

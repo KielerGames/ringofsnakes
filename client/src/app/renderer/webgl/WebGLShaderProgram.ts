6ffbe199539160bbd8169a213ce9befdb908111f
Tim Weißenfels <tim-we@users.noreply.github.com>
2022-09-15 01:24:25 +0200

    Switch to real private properties

--- client/src/app/renderer/webgl/WebGLShaderProgram.ts
+++ client/src/app/renderer/webgl/WebGLShaderProgram.ts
@@ -4,20 +4,20 @@ type ShaderVarValue = number | number[] | Float32Array;
 type WebGLAttributeLocation = number;
 
 export default class WebGLShaderProgram {
-    private gl: WebGLRenderingContext;
-    private program: WebGLProgram;
-    private uniforms: Map<string, ShaderVar<WebGLUniformLocation>> = new Map();
-    private attribs: Map<string, ShaderVar<WebGLAttributeLocation>> = new Map();
-    private attribOrder: string[];
-    private autoStride: number = 0;
+    readonly #gl: WebGLRenderingContext;
+    readonly #program: WebGLProgram;
+    readonly #uniforms: Map<string, ShaderVar<WebGLUniformLocation>> = new Map();
+    readonly #attribs: Map<string, ShaderVar<WebGLAttributeLocation>> = new Map();
+    #attribOrder: string[];
+    #autoStride: number = 0;
 
-    public constructor(
+    constructor(
         gl: WebGLRenderingContext,
         vertex: string,
         fragment: string,
         vertexBufferLayout?: string[]
     ) {
-        this.gl = gl;
+        this.#gl = gl;
 
         // compile & link shader program
         const vs = compileShader(gl, gl.VERTEX_SHADER, vertex);
@@ -42,7 +42,7 @@ export default class WebGLShaderProgram {
             }
         }
 
-        this.program = program;
+        this.#program = program;
 
         // get shader attributes & uniforms
         // attributes & uniforms are "active" after linking
@@ -53,7 +53,7 @@ export default class WebGLShaderProgram {
         for (let i = 0; i < numberOfAttributes; i++) {
             const info = gl.getActiveAttrib(program, i)!;
             const location = gl.getAttribLocation(program, info.name);
-            this.attribs.set(info.name, new ShaderVar(info, location));
+            this.#attribs.set(info.name, new ShaderVar(info, location));
             attribOrder.push(info.name);
         }
 
@@ -66,7 +66,7 @@ export default class WebGLShaderProgram {
                 }
 
                 for (const attribName of vertexBufferLayout) {
-                    if (!this.attribs.has(attribName)) {
+                    if (!this.#attribs.has(attribName)) {
                         throw new Error(
                             `Attribute "${attribName}" not found. Unused attributes get removed by the compiler.`
                         );
@@ -74,9 +74,9 @@ export default class WebGLShaderProgram {
                 }
             }
 
-            this.attribOrder = [...vertexBufferLayout];
+            this.#attribOrder = [...vertexBufferLayout];
         } else {
-            this.attribOrder = attribOrder;
+            this.#attribOrder = attribOrder;
         }
 
         this.computeStride();
@@ -88,19 +88,19 @@ export default class WebGLShaderProgram {
         for (let i = 0; i < numberOfUniforms; i++) {
             const info = gl.getActiveUniform(program, i)!;
             const location = gl.getUniformLocation(program, info.name)!;
-            this.uniforms.set(info.name, new ShaderVar(info, location));
+            this.#uniforms.set(info.name, new ShaderVar(info, location));
         }
     }
 
     private computeStride() {
-        const sum = Array.from(this.attribs.values())
+        const sum = Array.from(this.#attribs.values())
             .filter((attrib) => attrib.value === null)
             .reduce((s, attrib) => s + attrib.size, 0);
-        this.autoStride = sum * Float32Array.BYTES_PER_ELEMENT;
+        this.#autoStride = sum * Float32Array.BYTES_PER_ELEMENT;
     }
 
     public use(): void {
-        this.gl.useProgram(this.program);
+        this.#gl.useProgram(this.#program);
     }
 
     public run(
@@ -111,20 +111,20 @@ export default class WebGLShaderProgram {
             stride?: number;
         }
     ): void {
-        const gl = this.gl;
+        const gl = this.#gl;
 
         const { mode, start, stride } = Object.assign(
             {
-                mode: this.gl.TRIANGLES,
+                mode: this.#gl.TRIANGLES,
                 start: 0,
-                stride: this.autoStride
+                stride: this.#autoStride
             },
             options
         );
 
         let offset = 0;
-        for (const name of this.attribOrder) {
-            const attrib = this.attribs.get(name)!; // TODO?
+        for (const name of this.#attribOrder) {
+            const attrib = this.#attribs.get(name)!; // TODO?
             if (attrib.value === null) {
                 gl.enableVertexAttribArray(attrib.location);
                 gl.vertexAttribPointer(
@@ -149,7 +149,7 @@ export default class WebGLShaderProgram {
             }
         }
 
-        this.uniforms.forEach((uniform) => {
+        this.#uniforms.forEach((uniform) => {
             if (uniform.value !== null) {
                 if (uniform.type === gl.FLOAT) {
                     gl.uniform1f(uniform.location, uniform.value as number);
@@ -175,7 +175,7 @@ export default class WebGLShaderProgram {
     }
 
     public get context() {
-        return this.gl;
+        return this.#gl;
     }
 
     /**
@@ -184,7 +184,7 @@ export default class WebGLShaderProgram {
      * @param value use null to enable VertexAttribArray
      */
     public setAttribute(name: string, value: ShaderVarValue | null): void {
-        const attrib = this.attribs.get(name);
+        const attrib = this.#attribs.get(name);
 
         if (attrib === undefined) {
             throw new Error(`Attribute ${name} does not exist.`);
@@ -198,7 +198,7 @@ export default class WebGLShaderProgram {
     }
 
     public setUniform(name: string, value: ShaderVarValue): void {
-        const uniform = this.uniforms.get(name);
+        const uniform = this.#uniforms.get(name);
 
         if (uniform === undefined) {
             throw new Error(`Uniform ${name} does not exist.`);

6f1e78b5eb1d52495b3179e77a83abbfae09bc21
Tim Weißenfels <tim-we@users.noreply.github.com>
2022-10-30 01:27:47 +0200

    Remove WebGL code from FoodChunk.ts

--- client/src/app/data/world/FoodChunk.ts
+++ client/src/app/data/world/FoodChunk.ts
@@ -1,48 +1,22 @@
-import * as BufferManager from "../../renderer/webgl/BufferManager";
+import type { Consumer } from "../../util/FunctionTypes";
+import type { FoodChunkDTO } from "../dto/FoodChunkDTO";
+import type Camera from "../camera/Camera";
+import type { ManagedObject } from "../../util/ManagedMap";
 import Rectangle from "../../math/Rectangle";
-import { FoodChunkDTO } from "../dto/FoodChunkDTO";
-import Camera from "../camera/Camera";
 import * as FrameTime from "../../util/FrameTime";
-import { ManagedObject } from "../../util/ManagedMap";
 
-// TODO: move webgl stuff out of here
 export default class FoodChunk implements ManagedObject<number, FoodChunkDTO> {
     readonly id: number;
     readonly box: Rectangle;
 
-    #gpuBuffer: WebGLBuffer;
-    #gpuData: ArrayBuffer | undefined;
+    #vertexBuffer: ArrayBuffer | undefined;
     #numFoodItems: number;
     #lastUpdateTime: number;
 
     constructor(dto: FoodChunkDTO) {
         this.id = dto.id;
         this.box = Rectangle.fromTransferable(dto.bounds);
-        this.#gpuBuffer = BufferManager.create();
         this.update(dto);
-    }
-
-    update(dto: FoodChunkDTO): void {
-        this.#numFoodItems = dto.count;
-        this.#gpuData = dto.vertexBuffer;
-        this.#lastUpdateTime = FrameTime.now();
-    }
-
-    destroy(): void {
-        BufferManager.free(this.#gpuBuffer);
-    }
-
-    useBuffer(gl: WebGL2RenderingContext): void {
-        gl.bindBuffer(gl.ARRAY_BUFFER, this.#gpuBuffer);
-
-        if (this.#gpuData) {
-            gl.bufferData(gl.ARRAY_BUFFER, this.#gpuData, gl.STATIC_DRAW);
-            this.#gpuData = undefined;
-        }
-    }
-
-    isVisible(camera: Camera, eps: number = 0.0): boolean {
-        return Rectangle.distance2(this.box, camera.viewBox) <= eps * eps;
     }
 
     get length(): number {
@@ -54,5 +28,33 @@ export default class FoodChunk implements ManagedObject<number, FoodChunkDTO> {
      */
     get age(): number {
         return 0.001 * (FrameTime.now() - this.#lastUpdateTime);
+    }
+
+    update(dto: FoodChunkDTO): void {
+        this.#numFoodItems = dto.count;
+        this.#vertexBuffer = dto.vertexBuffer;
+        this.#lastUpdateTime = FrameTime.now();
+    }
+
+    destroy(): void {
+        // TODO
+    }
+
+    isVisible(camera: Camera, eps: number = 0.0): boolean {
+        return Rectangle.distance2(this.box, camera.viewBox) <= eps * eps;
+    }
+
+    /**
+     * If this FoodChunk contains new vertex data move it to the consumer.
+     */
+    moveVertexData(consumer: Consumer<ArrayBuffer>): void {
+        if (this.#vertexBuffer === undefined) {
+            return;
+        }
+
+        const data = this.#vertexBuffer;
+        this.#vertexBuffer = undefined;
+
+        consumer(data);
     }
 }

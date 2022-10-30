5126ee5cd5c6832205ed27f4ba22627e366a6dd8
Tim Weißenfels <tim-we@users.noreply.github.com>
2022-10-30 23:38:51 +0100

    Basic wiggling

--- client/src/app/worker/decoder/FoodDecoder.ts
+++ client/src/app/worker/decoder/FoodDecoder.ts
@@ -2,6 +2,7 @@
 
 import type { GameConfig } from "../../data/config/GameConfig";
 import type { FoodChunkDTO } from "../../data/dto/FoodChunkDTO";
+import assert from "../../util/assert";
 import type { DecodeResult } from "./DecodeResult";
 
 export const NUM_SKINS = 7;
@@ -11,7 +12,9 @@ const SIZE_BIT_OFFSET = 6;
 const COLOR_BIT_MASK = (1 << SIZE_BIT_OFFSET) - 1;
 const FOOD_SIZES = [0.64, 1.0, 1.5];
 const VERTEX_BYTE_SIZE =
-    3 * Float32Array.BYTES_PER_ELEMENT + // x,y, size
+    2 * Float32Array.BYTES_PER_ELEMENT + // position (x,y),
+    2 * Float32Array.BYTES_PER_ELEMENT + // wiggle parameters,
+    Float32Array.BYTES_PER_ELEMENT + // size,
     Int32Array.BYTES_PER_ELEMENT; // color
 
 export function decode(
@@ -22,36 +25,49 @@ export function decode(
     const decView = new DataView(buffer, offset);
     const chunkSize = config.chunks.size;
 
+    const n = decView.getUint16(2, false);
+
+    // Chunk related stuff.
+    const chunkId = decView.getUint16(0, false);
     const column = decView.getUint8(0);
     const row = decView.getUint8(1);
-    const n = decView.getUint16(2, false);
-    const chunkId = decView.getUint16(0, false);
-
     const xOffset = (column - 0.5 * config.chunks.columns) * chunkSize;
     const yOffset = (row - 0.5 * config.chunks.rows) * chunkSize;
 
+    // Output vars.
     const vertexBuffer = new ArrayBuffer(n * VERTEX_BYTE_SIZE);
     const encView = new DataView(vertexBuffer);
 
+    // Iterate over food items.
     for (let i = 0; i < n; i++) {
-        // decode
         const foodOffset = FOOD_CHUNK_HEADER_SIZE + i * FOOD_BYTE_SIZE;
+
+        // Unpack relative position.
         const bx = decView.getInt8(foodOffset + 0) + 128;
         const by = decView.getInt8(foodOffset + 1) + 128;
-        const colorAndSize = decView.getUint8(foodOffset + 2);
 
+        // Unpack color and size.
+        const colorAndSize = decView.getUint8(foodOffset + 2);
         const size = FOOD_SIZES[colorAndSize >> SIZE_BIT_OFFSET];
         const color = colorAndSize & COLOR_BIT_MASK;
 
+        // Compute absolute position.
         const x = xOffset + (bx / 256) * chunkSize;
         const y = yOffset + (by / 256) * chunkSize;
 
-        // encode vertex buffer
+        // Create seeds for random float generation based on the given
+        // bits s.t. they are consistent (same input => same output).
+        const seedA = bx ^ by ^ colorAndSize;
+        const seedB = rotateBits(seedA, 4);
+
+        // Encode data for vertex buffer.
         const vbOffset = i * VERTEX_BYTE_SIZE;
         encView.setFloat32(vbOffset + 0, x, true);
         encView.setFloat32(vbOffset + 4, y, true);
-        encView.setFloat32(vbOffset + 8, size, true);
-        encView.setInt32(vbOffset + 12, color % NUM_SKINS, true);
+        encView.setFloat32(vbOffset + 8, wiggleSpeed(seedA), true);
+        encView.setFloat32(vbOffset + 12, wiggleSpeed(seedB), true);
+        encView.setFloat32(vbOffset + 16, size, true);
+        encView.setInt32(vbOffset + 20, color % NUM_SKINS, true);
     }
 
     return {
@@ -68,4 +84,16 @@ export function decode(
         },
         nextByteOffset: offset + FOOD_CHUNK_HEADER_SIZE + n * FOOD_BYTE_SIZE
     };
+}
+
+function wiggleSpeed(seed: number): number {
+    const sign = 2 * (Math.clz32(seed) & 1) - 1;
+    assert(Math.abs(sign) === 1.0);
+    const epsilon = 0.3 * (seed / 256);
+    return sign * (1.0 + epsilon);
+}
+
+function rotateBits(bits: number, d: number): number {
+    d = d & 7;
+    return (bits << d) | (bits >> (8-d));
 }
--- client/src/shader/food.vert
+++ client/src/shader/food.vert
@@ -4,18 +4,21 @@ precision mediump float;
 
 in vec2 aPosition;
 in vec2 aLocalPos;
+in vec2 aWiggleParams;
 in int aColorIndex;
 in float aSize;
 
 uniform mat3 uTransform;
 uniform sampler2D uColorSampler;
 uniform vec2 uAttractorPosition;
+uniform float uTime;
 
 out vec2 vPos;
 flat out lowp vec3 vColor;
 out lowp float vOpacity;
 
 const float cMoveDist = 7.5;
+const float cWS = 0.8; // wiggle base speed
 
 void main(void) {
     vPos = aLocalPos;
@@ -26,7 +29,10 @@ void main(void) {
     float s = min(1.0, d * 0.25);
     vOpacity = min(1.0, d * 0.375);
 
+    float t0 = 10.0 * fract(0.23 * aPosition.x + 0.21 * aPosition.y);
+    vec2 wiggle = 0.3 * s * vec2(cos(aWiggleParams.x * cWS * uTime + t0), sin(aWiggleParams.y * cWS * uTime + t0));
+
     vec2 pos = (d < cMoveDist) ? mix(worldPosition, uAttractorPosition, 1.0 - s) : worldPosition;
 
-    gl_Position = vec4(uTransform * vec3(pos, 1.0), 1.0);
+    gl_Position = vec4(uTransform * vec3(pos + wiggle, 1.0), 1.0);
 }

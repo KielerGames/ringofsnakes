9750ac3aa3e088f83d2b4ec2e524a66ff56c3370
Tim Weißenfels <tim-we@users.noreply.github.com>
2022-11-03 15:50:05 +0100

    Use svgo to compress svg files

--- client/scripts/copy-assets.mjs
+++ client/scripts/copy-assets.mjs
@@ -1,5 +1,6 @@
 import fs from "node:fs";
 import path from "node:path";
+import svgo from "svgo";
 
 const TEXTURE_DIR = "src/textures";
 const OUTPUT_FOLDER = "public/assets";
@@ -12,7 +13,34 @@ if (!fs.existsSync(OUTPUT_FOLDER)) {
 fs.readdirSync(TEXTURE_DIR)
     .filter((file) => file.endsWith(".svg"))
     .forEach((texture) => {
-        console.log("copying " + texture);
-        fs.copyFileSync(path.join(TEXTURE_DIR, texture), path.join(OUTPUT_FOLDER, texture));
-        // TODO: use https://github.com/svg/svgo
+        const filePath = path.join(TEXTURE_DIR, texture);
+
+        if (texture.endsWith(".svg")) {
+            console.log("optimizing " + texture);
+            const originalSVG = fs.readFileSync(filePath, "utf-8");
+            const optimizedSVG = svgo.optimize(originalSVG, {
+                plugins: [
+                    "removeDoctype",
+                    "removeComments",
+                    "removeMetadata",
+                    "cleanupAttrs",
+                    "cleanupIds",
+                    "mergeStyles",
+                    "removeViewBox",
+                    "removeUselessStrokeAndFill",
+                    "minifyStyles",
+                    "removeEmptyAttrs",
+                    "convertColors",
+                    "removeUnknownsAndDefaults",
+                    "sortDefsChildren",
+                    "removeNonInheritableGroupAttrs",
+                    "removeHiddenElems"
+                ],
+                multipass: true
+            }).data;
+            fs.writeFileSync(path.join(OUTPUT_FOLDER, texture), optimizedSVG, "utf-8");
+        } else {
+            console.log("copying " + texture);
+            fs.copyFileSync(filePath, path.join(OUTPUT_FOLDER, texture));
+        }
     });

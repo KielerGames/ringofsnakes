08db5c39e3ed0589265aa439782498d6b71b258e
Tim Weißenfels <tim-we@users.noreply.github.com>
2022-11-09 23:44:48 +0100

    Fix matrix tests

--- client/test/math/Matrix.test.ts
+++ client/test/math/Matrix.test.ts
@@ -9,16 +9,16 @@ describe("Matrix", () => {
 
         const v1 = new Vector(42.0, 13.7);
         const v2 = matrix.multiply(v1);
-        expect(Vector.equals(v1, v2, 1e-8));
+        expect(Vector.equals(v1, v2, 1e-8)).toBe(true);
     });
 
-    test("identity matrix", () => {
+    test("zero matrix", () => {
         const matrix = new Matrix(false);
         expect(matrix.getEntry(0, 0)).toBe(0);
         expect(matrix.getEntry(1, 1)).toBe(0);
 
-        const v1 = new Vector(42.0, 13.7);
-        const v2 = matrix.multiply(v1);
-        expect(Vector.equals(v1, v2, 1e-8));
+        const v = new Vector(42.0, 13.7);
+        const zero = new Vector(0.0, 0.0);
+        expect(Vector.equals(matrix.multiply(v), zero, 1e-8)).toBe(true);
     });
 });

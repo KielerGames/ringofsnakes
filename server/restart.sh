627b87016351d7ead119f80750ef9b5668881f7f
Tim Weißenfels <tim-we@users.noreply.github.com>
2022-11-18 21:43:01 +0100

    Update restart.sh script

--- server/restart.sh
+++ server/restart.sh
@@ -5,6 +5,10 @@
 
 # stop previous versions
 pkill -f java
+sleep 1s
+
+# remove dead screen sessions
+screen -wipe
 
 # start server in screen
 screen -d -m -S snakeserver java -jar snake-server.jar

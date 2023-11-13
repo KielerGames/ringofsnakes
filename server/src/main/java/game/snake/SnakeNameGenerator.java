1157d7d867ce4105ece847a0590847b132456bfb
Tim Weißenfels <10491533+tim-we@users.noreply.github.com>
2023-11-13 20:24:55 +0100

    More bot names

--- server/src/main/java/game/snake/SnakeNameGenerator.java
+++ server/src/main/java/game/snake/SnakeNameGenerator.java
@@ -13,11 +13,11 @@ public class SnakeNameGenerator {
     private static final String[] adjectives = {
             "Abnormal", "Aimless", "Alien", "Alpha", "Amazing", "Angry",
             "Annoying", "Apt", "Armless", "Average", "Bad", "Baffling", "Bare",
-            "Basic", "Bitter", "Busy", "Capable", "Chunky", "Clumsy", "Crawling",
-            "Crazy", "Dangerous", "Deadly", "Diabolic", "Dizzy", "Eager", "Elite",
-            "Emo", "Evil", "Exotic", "Fabulous", "Fake", "Fancy", "Fast", "Fatal",
-            "Fatal", "Fierce", "Flailing", "Foolish", "Frisky", "Furious", "Giddy",
-            "Goofy", "Grand", "Great", "Greedy", "Grim", "Hasty", "Hateful",
+            "Basic", "Bitter", "Busy", "Capable", "Chunky", "Clumsy", "Common",
+            "Crawling", "Crazy", "Dangerous", "Deadly", "Diabolic", "Dizzy", "Eager",
+            "Elite", "Emo", "Evil", "Exotic", "Fabulous", "Fake", "Fancy", "Fast",
+            "Fatal", "Fatal", "Fierce", "Flailing", "Foolish", "Frisky", "Furious",
+            "Giddy", "Goofy", "Grand", "Great", "Greedy", "Grim", "Hasty", "Hateful",
             "Heavy", "Huge", "Hungry", "Iconic", "Lazy", "Long", "Lucky", "Mad",
             "Magnificent", "Massive", "Mighty", "Mystic", "Nasty", "Nervous",
             "Noble", "Quick", "Raging", "Savage", "Scaly", "Silly", "Slithering",
@@ -26,7 +26,7 @@ public class SnakeNameGenerator {
     };
 
     private static final String[] nouns = {
-            "Adder", "Anaconda", "Animal", "Basilisk", "Cobra", "Colubrid",
+            "Adder", "Anaconda", "Animal", "Basilisk", "Boa", "Cobra", "Colubrid",
             "Constrictor", "Crawler", "Creature", "Danger", "Guy", "Joker", "Line",
             "Maggot", "Mamba", "Monster", "Noodle", "Python", "Reptile", "Serpent",
             "Snake", "Snek", "Specimen", "Tube", "Villain", "Viper", "Wire", "Worm"
@@ -51,6 +51,10 @@ public class SnakeNameGenerator {
 
         if (name.length() >= 10 && rand.nextBoolean()) {
             return name;
+        }
+
+        if (name.length() <= 11 && name.endsWith("Boa")) {
+            return name + "Constrictor";
         }
 
         final var ps = Arrays.stream(prefixes)
@@ -93,6 +97,6 @@ public class SnakeNameGenerator {
         Random rand = ThreadLocalRandom.current();
         final var adjective = adjectives[rand.nextInt(adjectives.length)];
         final var noun = botNouns[rand.nextInt(botNouns.length)];
-        return adjective + noun + " " + rand.nextInt(100,999);
+        return adjective + noun + " " + rand.nextInt(100, 999);
     }
 }
